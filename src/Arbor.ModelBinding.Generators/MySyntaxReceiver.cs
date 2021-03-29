8d946be1736e09a64ed2adbc068b2591f52513ca
Niklas Lundberg <info@nlundberg.se>
2021-03-29 13:53:43 +0200

    Added check for supported attribute.

--- src/Arbor.ModelBinding.Generators/MySyntaxReceiver.cs
+++ src/Arbor.ModelBinding.Generators/MySyntaxReceiver.cs
@@ -11,11 +11,15 @@ namespace Arbor.ModelBinding.Generators
     {
         public List<ClassData> CommandsToGenerateFor { get; } = new ();
 
+        private readonly HashSet<string> _supportedDataTypes =
+            new(StringComparer.OrdinalIgnoreCase) {"int", "string"};
+
         public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
         {
             if (syntaxNode is ClassDeclarationSyntax cds
-                && cds.IsPartial() && cds.DataType() is {} dataType)
-
+                && cds.IsPartial()
+                && cds.DataType() is {} dataType
+                && _supportedDataTypes.Contains(dataType))
             {
                 CommandsToGenerateFor.Add(new ClassData(cds, cds.Namespace(), dataType));
             }
