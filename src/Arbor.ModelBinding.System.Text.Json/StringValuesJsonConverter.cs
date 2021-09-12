0534b40ac237fb1140a84e520bf59e8186306af4
Niklas Lundberg <info@nlundberg.se>
2021-09-12 11:02:04 +0200

    Cleanup.

---
+++ src/Arbor.ModelBinding.System.Text.Json/StringValuesJsonConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Primitives;
+
+namespace Arbor.ModelBinding.SystemTextJson
+{
+    public class StringValuesJsonConverter : JsonConverter<StringValues>
+    {
+        public override StringValues Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.StartArray)
+            {
+                string[]? values = JsonSerializer.Deserialize<string[]>(ref reader, options);
+
+                if (values is null || values.Length == 0)
+                {
+                    return StringValues.Empty;
+                }
+
+                if (values.Length == 1)
+                {
+                    return new StringValues(values[0]);
+                }
+
+                return new StringValues(values);
+            }
+
+            return StringValues.Empty;
+        }
+
+        public override void Write(Utf8JsonWriter writer, StringValues value, JsonSerializerOptions options)
+        {
+            if (value.Count == 0)
+            {
+                writer.WriteStringValue("");
+            }
+
+            if (value.Count == 1)
+            {
+                writer.WriteStringValue(value[0]);
+            }
+
+            if (value.Count > 1)
+            {
+                writer.WriteStringValue(string.Join(", ", value));
+            }
+        }
+    }
+}
